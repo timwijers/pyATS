35ba935ff86ad7995c9fff864557724febac8147
timwijers <tim.wijers@student.pxl.be>
2020-04-15 16:42:45 +0200

    mod tc's

--- testtrigger1.py
+++ testtrigger1.py
@@ -50,7 +50,7 @@ class test_cases (aetest.Testcase):
     @aetest.test
     def conf_int_e01_cmd(self, uut):
         # Get device output
-        uut.configure("interface ethernet0/1\n" " ip address 192.168.1.5 255.2355.255.0\n" " no sh\n")
+        uut.configure("interface ethernet0/1\n" " ip address 192.168.1.5 255.255.255.0\n" " no sh\n")
         time.sleep(15)
         check = uut.execute('show ip int brief ethernet0/1')
         if check[ip_address] != '192.168.1.5': self.failed("wrong ip address")
