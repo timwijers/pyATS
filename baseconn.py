e260be20f15bcc52f2742a09f9db2867efa88bc1
timwijers <tim.wijers@student.pxl.be>
2020-04-15 13:01:57 +0200

    baseconn.py added

---
+++ baseconn.py
@@ -0,0 +1,8 @@
+from pyats.topology import loader
+tb = loader.load('cisco.yaml')
+dev = tb.devices['router1']
+devxe = tb.devices['routerXE']
+dev.connect(alias='uut', via='cli')
+devxe.connect(alias='uutxe', via='netconf')
+print(dev.uut.connected)
+print(devxe.uutxe.connected)
