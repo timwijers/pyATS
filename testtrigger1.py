3d9d058c5fcc8bb112045a68416e9d9615978936
timwijers <tim.wijers@student.pxl.be>
2020-04-15 16:25:29 +0200

    mod to tc's

--- testtrigger1.py
+++ testtrigger1.py
@@ -55,6 +55,8 @@ class test_cases (aetest.Testcase):
         uut.execute('ip address 192.168.1.5 255.2355.255.0')
         uut.execute('no sh')
         uut.execute('exit')
+        uut.execute('end')
+        #uut.execute('exit')
         check = uut.parse('show ip int brief ethernet0/1')
         if check[ip_address] != '192.168.1.5': self.failed("wrong ip address")
 
