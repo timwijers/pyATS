de594febc902987ae7f9914e57bd8055461b49da
timwijers <tim.wijers@student.pxl.be>
2020-04-15 13:15:57 +0200

    testtrigger1job added and edited

---
+++ testtrigger1job.py
@@ -0,0 +1,10 @@
+import os
+
+from genie.harness.main import gRun
+from pyats.easypy import run
+
+def main():
+     test_path = os.path.dirname(os.path.abspath(__file__))
+     testscript = os.path.join(test_path, 'testtrigger1.py')
+
+     run(testscript=testscript)
