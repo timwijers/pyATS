8c1135a460618432bbb8eeea9ecc7533e726c456
timwijers <tim.wijers@student.pxl.be>
2020-04-16 15:11:28 +0200

    mod connect

--- baseconn.py
+++ baseconn.py
@@ -1,5 +1,5 @@
 from pyats.topology import loader
-tb = loader.load('cisco.yaml')
+tb = loader.load('routerIOL_tb.yaml')
 dev = tb.devices['router1']
 devxe = tb.devices['routerXE']
 dev.connect(alias='uut', via='cli')
--- jobfile.py
+++ jobfile.py
@@ -5,6 +5,6 @@ from pyats.easypy import run
 
 def main():
      test_path = os.path.dirname(os.path.abspath(__file__))
-     testscript = os.path.join(test_path, 'testtrigger1.py')
+     testscript = os.path.join(test_path, 'set_check_ip_tcs.py')
 
      run(testscript=testscript)
