cdbd96b899f1dd11f7be7857c3ec72c77964b522
timwijers <tim.wijers@student.pxl.be>
2020-04-17 12:40:30 +0200

    mod

--- configdev.py
+++ configdev.py
@@ -21,8 +21,6 @@ routeriol1.default
 routeriol2.connect()
 routeriol2.default
 
-routeriol1.default.execute('show ip int brief')
-routeriol2.default.execute('show ip int brief')
 
 routeriol1.default.execute("interface ethernet0/2\n" " ip address 192.168.2.2 255.255.255.0\n" " no sh\n")
 routeriol2.default.execute("interface ethernet0/2\n" " ip address 192.168.2.3 255.255.255.0\n" " no sh\n")
