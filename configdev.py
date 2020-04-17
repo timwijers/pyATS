bc06875bdf69761f03d5a0d4ad783350a2b57640
timwijers <tim.wijers@student.pxl.be>
2020-04-17 12:25:28 +0200

    mod

--- configdev.py
+++ configdev.py
@@ -13,9 +13,11 @@ testbed = topology.loader.load('routerIOL_tb.yaml')
 # pick a device
 
 routeriol1 = testbed.devices['routeriol']
+routeriol2 = testbed.devices['routeriol2']
 
 routeriol1.connect()
-routeriol1.is_connected()
-# True
 routeriol1.default
+routeriol2.connect()
+routeriol2.default
 routeriol1.default.execute('show ip int brief')
+routeriol2.default.execute('show ip int brief')
