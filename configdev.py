02c3915b7e8af2d8f38d608dd37b7353ec1edae7
timwijers <tim.wijers@student.pxl.be>
2020-04-17 12:58:21 +0200

    mod

--- configdev.py
+++ configdev.py
@@ -6,28 +6,21 @@
 # using the sample topology file from
 import os
 from pyats import topology
+from genie.testbed import load
 
-
-testbed = topology.loader.load('routerIOL_tb.yaml')
-
-# pick a device
+testbed = load('routerIOL_tb.yaml')
 
 routeriol1 = testbed.devices['routeriol']
 routeriol2 = testbed.devices['routeriol2']
 
 routeriol1.connect()
-routeriol1.default
 
 routeriol2.connect()
-routeriol2.default
 
+routeriol1.configure("interface ethernet0/2\n" " ip address 192.168.3.2 255.255.255.0\n" " no sh\n")
+routeriol2.configure("interface ethernet0/2\n" " ip address 192.168.3.3 255.255.255.0\n" " no sh\n")
 
-routeriol1.default.configure("interface ethernet0/2\n" " ip address 192.168.2.2 255.255.255.0\n" " no sh\n")
-routeriol2.default.configure("interface ethernet0/2\n" " ip address 192.168.2.3 255.255.255.0\n" " no sh\n")
+routeriol1.execute('show ip int brief')
+routeriol2.execute('show ip int brief')
 
-routeriol1.default.execute('show ip int brief')
-routeriol2.default.execute('show ip int brief')
-
-out = routeriol1.parse('show ip interface brief ethernet0/2')
-
-print(out)
+routeriol1.parse('show ip interface brief ethernet0/2')
