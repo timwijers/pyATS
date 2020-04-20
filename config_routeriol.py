d1c2b757be6d8f7d7c27212bfa63be32a1270425
timwijers <tim.wijers@student.pxl.be>
2020-04-20 13:26:23 +0200

    mod

--- config_routeriol.py
+++ config_routeriol.py
@@ -49,14 +49,14 @@ class test_cases(aetest.Testcase):
         uut.configure("interface ethernet0/1\n" " ip address 192.168.2.1 255.255.255.0\n" " no sh\n")
         uut.configure("interface serial1/1\n" " ip address 10.0.2.1 255.255.255.254\n" " no sh\n")
         uut.configure("interface serial1/2\n" " ip address 10.0.3.1 255.255.255.254\n" " no sh\n")
-        uut.configure(" ip route 0.0.0.0 0.0.0.0 ethernet0/0\n" " end\n")
+        #uut.configure(" ip route 0.0.0.0 0.0.0.0 ethernet0/0\n" " end\n")
         uut.configure("router eigrp 1\n" "eigrp router-id 1.1.1.1\n"
                       "network 10.0.2.0\n"  " no auto-summary\n " "redistribute static\n" " network 10.0.3.0\n "
                       "network 192.168.5.0\n" "network 192.168.2.0\n" "passive-interface ethernet0/2\n "
                       "passive-interface ethernet0/0\n " "passive-interface ethernet0/1\n " " exit\n")
 
         uut2.configure("interface ethernet0/2\n" " ip address 192.168.6.1 255.255.255.0\n" " no sh\n")
-        uut2.configure("interface ethernet0/1\n" " ip address 192.168.3.1 255.255.255.0\n" " no sh\n")
+        uut2.configure("interface ethernet0/1\n" " ip address 192.168.3.1 255.255.255.0\n" " no sh\n"
         uut2.configure("interface serial1/1\n" " ip address 10.0.2.2 255.255.255.254\n" " no sh\n")
         uut2.configure("interface serial1/0\n" " ip address 10.0.1.1 255.255.255.254\n" " no sh\n")
         uut2.configure("router eigrp 1\n" "eigrp router-id 2.2.2.2\n"" network 10.0.2.0\n" " network 10.0.1.0\n" "no "
