403a597c1573ad374f8ad45e9dac418c740befe3
timwijers <tim.wijers@student.pxl.be>
2020-04-22 11:50:14 +0200

    1

--- installscript.sh
+++ installscript.sh
@@ -4,15 +4,12 @@
 # Copyright (c) Routz B.V
 # Script follows here:
 
-apt-get update
-apt-get upgrade
+#apt-get update
+#apt-get upgrade
 
-apt-get install git
-git clone http://github.com/timwijers/pyATS
+cd /pyats/pyATS
+git pull http://github.com/timwijers/pyATS
 
-cd /pyats
-source bin/activate
-touch testfile.txt
 pyats run job jobfile.py
 
 
