32bd88b4ec0527a1760d147d85d46321b2e1caca
timwijers <tim.wijers@student.poxl.be>
2020-04-28 15:46:18 +0200

    add api python file

--- Configure Eve-ng Labs.py
+++ Configure Eve-ng Labs.py
@@ -5,6 +5,7 @@ loginCreds = {"username":"tim_wijers","password":"Welkom01"}
 login = requests.get('http://10.100.244.1/api/auth/login', params=loginCreds)
 
 if login.status_code != 200:
-    raise custom_error.APIError('GET /tasks/ {}'.format(login.status_code))
-for todo_item in login.json():
-    print('{} {}'.format(todo_item['id'], todo_item['summary']))
+    print('status not 200')
+
+for item in login.json():
+    print('{} {}'.format(item['id'], item['summary']))
