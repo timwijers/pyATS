dc6cfcb469eac6f8e40e430f923c41fc35f23390
timwijers <tim.wijers@student.poxl.be>
2020-04-28 15:47:46 +0200

    add api python file

--- Configure Eve-ng Labs.py
+++ Configure Eve-ng Labs.py
@@ -7,5 +7,3 @@ login = requests.get('http://10.100.244.1/api/auth/login', params=loginCreds)
 if login.status_code != 200:
     print('status not 200')
 
-for item in login.json():
-    print('{} {}'.format(item['id'], item['summary']))
