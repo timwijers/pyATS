a6b06738b5405991387bd122426d174ad87858de
timwijers <tim.wijers@student.poxl.be>
2020-04-29 12:11:35 +0200

    add api python file

--- Configure Eve-ng Labs.py
+++ Configure Eve-ng Labs.py
@@ -14,8 +14,8 @@ print(loginReq.json())
 
 # Create New Testlab in the pyATS_TestLabs folder #
 DateTimeObj = datetime.datetime.now()
-LabConfigData = '{"path":"/Tim Wijers/pyATS_TestLabs/","name":"pyATSTestLab_7","version":"1","author":"Tim Wijers",' \
-                '"description":"pyATS Test Lab 7","body":"This is a testlab created by the Eve-NG REST API. Intended ' \
+LabConfigData = '{"path":"/Tim Wijers/pyATS_TestLabs/","name":"pyATSTestLab_8","version":"1","author":"Tim Wijers",' \
+                '"description":"pyATS Test Lab 8","body":"This is a testlab created by the Eve-NG REST API. Intended ' \
                 'for pyATS framework testing purposes"} '
 
 LabConfigUrl = 'http://10.100.244.1/api/labs'
@@ -23,17 +23,33 @@ LabConfigReq = session.post(LabConfigUrl, LabConfigData)
 print(LabConfigReq.json())
 
 
-# Add a new node to the lab #
+# Add router 1 to the lab #
 NodeAddData = '{"type":"iol","template":"iol","config":"Unconfigured","delay":0,"icon":"Router.png",' \
+              '"image":"i86bi_LinuxL3-AdvEnterpriseK9-M2_157_3_May_2018.bin","name":"pyats_Router1","left":"35%",' \
+              '"top":"25%","ram":"1024","cpu":1,"ethernet":2, "nvram": 1024, "serial": 1} '
+NodeAddUrl = 'http://10.100.244.1/api/labs/Tim Wijers/pyATS_TestLabs/pyATSTestLab_8.unl/nodes'
+NodeAddReq = session.post(NodeAddUrl, NodeAddData)
+print(NodeAddReq.json())
+
+# Add router 2 to the lab #
+NodeAddData = '{"type":"iol","template":"iol","config":"Unconfigured","delay":0,"icon":"Router.png",' \
+              '"image":"i86bi_LinuxL3-AdvEnterpriseK9-M2_157_3_May_2018.bin","name":"pyats_Router2","left":"35%",' \
+              '"top":"25%","ram":"1024","cpu":1,"ethernet":2, "nvram": 1024, "serial": 1} '
+NodeAddUrl = 'http://10.100.244.1/api/labs/Tim Wijers/pyATS_TestLabs/pyATSTestLab_8.unl/nodes'
+NodeAddReq = session.post(NodeAddUrl, NodeAddData)
+print(NodeAddReq.json())
+
+# Add router 3 to the lab #
+NodeAddData = '{"type":"iol","template":"iol","config":"Saved","delay":0,"icon":"Router.png",' \
               '"image":"i86bi_LinuxL3-AdvEnterpriseK9-M2_157_3_May_2018.bin","name":"pyats_Router3","left":"35%",' \
               '"top":"25%","ram":"1024","cpu":1,"ethernet":2, "nvram": 1024, "serial": 1} '
-NodeAddUrl = 'http://10.100.244.1/api/labs/Tim Wijers/pyATS_TestLabs/pyATSTestLab_7.unl/nodes'
+NodeAddUrl = 'http://10.100.244.1/api/labs/Tim Wijers/pyATS_TestLabs/pyATSTestLab_8.unl/nodes'
 NodeAddReq = session.post(NodeAddUrl, NodeAddData)
 print(NodeAddReq.json())
 
 # Create a new Internet Gateway for internet connection #
 NetworkAddData = '{"type":"bridge","name":"InternetGateway","left":"35%","top":"25%"}'
-NetworkAddUrl = 'http://10.100.244.1/api/labs/Tim Wijers/pyATS_TestLabs/pyATSTestLab_7.unl/networks'
+NetworkAddUrl = 'http://10.100.244.1/api/labs/Tim Wijers/pyATS_TestLabs/pyATSTestLab_8.unl/networks'
 NetworkAddReq = session.post(NetworkAddUrl, NetworkAddData)
 print(NetworkAddReq.json())
 
