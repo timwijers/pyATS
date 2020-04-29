36db83812047fdd1191bd29457bcc06bc70f133c
timwijers <tim.wijers@student.poxl.be>
2020-04-29 11:04:55 +0200

    add api python file

--- Configure Eve-ng Labs.py
+++ Configure Eve-ng Labs.py
@@ -1,9 +1,9 @@
 import requests
 import custom_error
 
-payload = {'username': 'tim_wijers', 'password': 'Welkom01'}
+data = '{"username":"admin","password":"eve"}'
 url = 'http://10.100.244.1/api/auth/login'
-login = requests.post(url, payload)
+login = requests.post(url,data)
 
 if login.status_code != 200:
     print('status not 200')
