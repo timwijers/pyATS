d59adbefc810b30e6a625554514053bbcf490433
timwijers <tim.wijers@student.poxl.be>
2020-04-29 11:25:15 +0200

    add api python file

--- Configure Eve-ng Labs.py
+++ Configure Eve-ng Labs.py
@@ -7,9 +7,6 @@ import custom_error
 # Login to Eve-NG #
 LoginData = '{"username":"tim_wijers","password":"Welkom01"}'
 LoginUrl = 'http://10.100.244.1/api/auth/login'
-loginReq = requests.post(LoginUrl, LoginData)
-print(loginReq.json())
-
 
 # Create New Testlab in the pyATS_TestLabs folder #
 DateTimeObj = datetime.datetime.now()
@@ -19,5 +16,6 @@ LabConfigData = '{"path":"/Tim Wijers/pyATS_TestLabs/","name":"pyATSTestLab_2","
 
 LabConfigUrl = 'http://10.100.244.1/api/labs'
 loginReq = requests.post(LoginUrl, LoginData)
+print(loginReq.json())
 LabConfigReq = requests.post(LabConfigUrl, LabConfigData)
 print(LabConfigReq.json())
