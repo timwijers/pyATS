52e1bdf089f69d45cea60f3938b13fd5c3d091e8
timwijers <tim.wijers@student.poxl.be>
2020-04-29 11:21:33 +0200

    add api python file

--- Configure Eve-ng Labs.py
+++ Configure Eve-ng Labs.py
@@ -13,9 +13,9 @@ print(loginReq.json())
 
 # Create New Testlab in the pyATS_TestLabs folder #
 DateTimeObj = datetime.datetime.now()
-LabConfigData = '{"path":"/Tim Wijers/pyATS_TestLabs/","name":"pyATSTestLab_{}","version":"1","author":"Tim Wijers",' \
-                '"description":"pyATS Test Lab_{}","body":"This is a testlab created by the Eve-NG REST API. Intended ' \
-                'for pyATS framework testing purposes"} '.format(DateTimeObj, DateTimeObj)
+LabConfigData = '{"path":"/Tim Wijers/pyATS_TestLabs/","name":"pyATSTestLab_2","version":"1","author":"Tim Wijers",' \
+                '"description":"pyATS Test Lab_2","body":"This is a testlab created by the Eve-NG REST API. Intended ' \
+                'for pyATS framework testing purposes"} '
 
 LabConfigUrl = 'http://10.100.244.1/api/labs'
 LabConfigReq = requests.post(LabConfigUrl, LabConfigData)
