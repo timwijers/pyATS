982e6878171214fa00ee7138b2726fb5de487b3f
timwijers <tim.wijers@student.poxl.be>
2020-05-05 15:17:19 +0200

    add api python file

--- IpExtractor.py
+++ IpExtractor.py
@@ -4,6 +4,9 @@ import time
 
 
 def extractip(ip, port, interface):
+    import time
+    import telnetlib
+    import re
     ipRegexPatt = re.compile('(([2][5][0-5]\.)|([2][0-4][0-9]\.)|([0-1]?[0-9]?[0-9]\.)){3}'
                              + '(([2][5][0-5])|([2][0-4][0-9])|([0-1]?[0-9]?[0-9]))')
 
@@ -24,8 +27,8 @@ def extractip(ip, port, interface):
     telNetSession.close()
 
     ExtractedIpAddress = ipRegexPatt.search(ResultString)
-    res = ExtractedIpAddress.group()
-    return res
+    res = ExtractedIpAddress
+    return ResultString
 
 
 class IpExtractorClass:
