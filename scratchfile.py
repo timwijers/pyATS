8b99d64a08b2359c4fdc5778dafbb9b6df7a3567
timwijers <tim.wijers@student.poxl.be>
2020-05-05 13:53:22 +0200

    add api python file

--- scratchfile.py
+++ scratchfile.py
@@ -1,6 +1,7 @@
 import telnetlib
 import re
-'''
+import time
+
 IpRegexPatt= re.compile('(([2][5][0-5]\.)|([2][0-4][0-9]\.)|([0-1]?[0-9]?[0-9]\.)){3}'
                 +'(([2][5][0-5])|([2][0-4][0-9])|([0-1]?[0-9]?[0-9]))')
 
@@ -8,20 +9,17 @@ TelNetSession = telnetlib.Telnet()
 
 TelNetSession.open('10.100.244.1', '45569')
 
+time.sleep(5)
+
 TelNetSession.write('enable\n')
 TelNetSession.write('show ip int brief ethernet0/0\n')
 
+time.sleep(5)
+
 ResultString = TelNetSession.read_very_eager()
-#TelNetSession.close()
+TelNetSession.close()
 
 ExtractedIpAddress = IpRegexPatt.search(ResultString)
 print(ResultString)
 print(ExtractedIpAddress)
-'''
-import telnetlib
-tn= telnetlib.Telnet()
-tn.open('10.100.244.1', '45569')
-tn.write('enable\n')
-tn.write('show ip int brief ethernet0/0\n')
-res = tn.read_very_eager()
-print(res)
+
