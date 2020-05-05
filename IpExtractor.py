6ecb5b69f9cbc30fda5247cfcf8f5609e2e4aaa1
timwijers <tim.wijers@student.poxl.be>
2020-05-05 14:52:12 +0200

    add api python file

--- IpExtractor.py
+++ IpExtractor.py
@@ -3,25 +3,25 @@ import re
 import time
 
 
-class IpExtractorClass:
 
-    def extractip(self, ip, port, interface):
-        regexPatt = re.compile('(([2][5][0-5]\.)|([2][0-4][0-9]\.)|([0-1]?[0-9]?[0-9]\.)){3}'
-                               + '(([2][5][0-5])|([2][0-4][0-9])|([0-1]?[0-9]?[0-9]))')
-        telNetSession = telnetlib.Telnet()
+ipRegexPatt = re.compile('(([2][5][0-5]\.)|([2][0-4][0-9]\.)|([0-1]?[0-9]?[0-9]\.)){3}'
+                                 + '(([2][5][0-5])|([2][0-4][0-9])|([0-1]?[0-9]?[0-9]))')
 
-        # telNetSession.open(ip, port)
-        telNetSession.open('10.100.244.1', '45569')
-        time.sleep(5)
+telNetSession = telnetlib.Telnet()
 
-        telNetSession.write('enable\n')
-        telNetSession.write('show ip int brief ethernet 0/0\n')
-        # telNetSession.write('show ip int brief' + interface + '\n')
-        time.sleep(5)
+telNetSession.open('10.100.244.1', '45569')
 
-        ResultString = telNetSession.read_very_eager()
-        print('Resultaat' + ResultString)
-        ExtractedIpAddress = regexPatt.search(ResultString)
-        #telNetSession.close()
+time.sleep(2)
 
-        return ResultString
+telNetSession.write('enable\n')
+telNetSession.write('show ip int brief ethernet0/0\n')
+
+time.sleep(2)
+
+ResultString = telNetSession.read_very_eager()
+telNetSession.close()
+
+ExtractedIpAddress = ipRegexPatt.search(ResultString)
+
+print(ResultString)
+print(ExtractedIpAddress.group())
--- scratchfile.py
+++ scratchfile.py
@@ -1,27 +1,3 @@
 from IpExtractor import IpExtractorClass
-import telnetlib
-import re
-import time
 
-'''
-ip1 = IpExtractorClass()
-res = ip1.extractip('10.100.244.1', '45569', 'ethernet0/0')
-print (res)
-'''
-regexPatt = re.compile('(([2][5][0-5]\.)|([2][0-4][0-9]\.)|([0-1]?[0-9]?[0-9]\.)){3}'
-                       + '(([2][5][0-5])|([2][0-4][0-9])|([0-1]?[0-9]?[0-9]))')
-telNetSession = telnetlib.Telnet()
-# telNetSession.open(ip, port)
-telNetSession.open('10.100.244.1', '45569')
-time.sleep(5)
-
-telNetSession.write('enable\n')
-telNetSession.write('show ip int brief ethernet0/0\n')
-# telNetSession.write('show ip int brief' + interface + '\n')
-time.sleep(5)
-
-ResultString = telNetSession.read_very_eager()
-time.sleep(5)
-print('Resultaat' + ResultString)
-ExtractedIpAddress = regexPatt.search(ResultString)
-# telNetSession.close()
+IpExtractorClass.extractip('')
