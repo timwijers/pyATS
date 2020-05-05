fc79a5c6b164b9f1edfd82742d17743091a362f8
timwijers <tim.wijers@student.poxl.be>
2020-05-05 15:08:07 +0200

    add api python file

--- IpExtractor.py
+++ IpExtractor.py
@@ -12,13 +12,16 @@ TelNetSession.open('10.100.244.1', '45569')
 time.sleep(5)
 
 TelNetSession.write('enable\n')
+time.sleep(5)
 TelNetSession.write('show ip int brief ethernet0/0\n')
 
 time.sleep(5)
 
 ResultString = TelNetSession.read_very_eager()
-TelNetSession.close()
+
 
 ExtractedIpAddress = IpRegexPatt.search(ResultString)
 print(ResultString)
 print(ExtractedIpAddress)
+
+
