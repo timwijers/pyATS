eb30d94c92ba47bea9a5b4266783c362312ca6f1
timwijers <tim.wijers@student.poxl.be>
2020-05-05 14:58:54 +0200

    add api python file

--- IpExtractor.py
+++ IpExtractor.py
@@ -2,28 +2,23 @@ import telnetlib
 import re
 import time
 
+IpRegexPatt= re.compile('(([2][5][0-5]\.)|([2][0-4][0-9]\.)|([0-1]?[0-9]?[0-9]\.)){3}'
+                +'(([2][5][0-5])|([2][0-4][0-9])|([0-1]?[0-9]?[0-9]))')
 
+TelNetSession = telnetlib.Telnet()
 
-ipRegexPatt = re.compile('(([2][5][0-5]\.)|([2][0-4][0-9]\.)|([0-1]?[0-9]?[0-9]\.)){3}'
-                                 + '(([2][5][0-5])|([2][0-4][0-9])|([0-1]?[0-9]?[0-9]))')
-
-telNetSession = telnetlib.Telnet()
-
-telNetSession.open('10.100.244.1', '45569')
+TelNetSession.open('10.100.244.1', '45569')
 
 time.sleep(5)
 
-telNetSession.write('enable\n')
-telNetSession.write('show ip int brief ethernet0/0\n')
+TelNetSession.write('enable\n')
+TelNetSession.write('show ip int brief ethernet0/0\n')
 
 time.sleep(5)
 
-ResultString = telNetSession.read_very_eager()
-print(ResultString)
-time.sleep(5)
-telNetSession.close()
+ResultString = TelNetSession.read_very_eager()
+TelNetSession.close()
 
-ExtractedIpAddress = ipRegexPatt.search(ResultString)
-
+ExtractedIpAddress = IpRegexPatt.search(ResultString)
 print(ResultString)
 print(ExtractedIpAddress)
