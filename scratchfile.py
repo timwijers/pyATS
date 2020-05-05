02c3118be346a56d4e79c1c41ce9a57dbe09ca2c
timwijers <tim.wijers@student.poxl.be>
2020-05-05 15:35:04 +0200

    add api python file

--- scratchfile.py
+++ scratchfile.py
@@ -1,5 +1,9 @@
 from IpExtractor import extractip
-import re
+
 
 ip1 = extractip('10.100.244.1', '45569', 'ethernet0/0')
-print(ip1)
+ip2 = extractip('10.100.244.1', '45570', 'ethernet0/0')
+ip3 = extractip('10.100.244.1', '45571', 'ethernet0/0')
+print('pyATS_Router 1 IP-Adres: ' + ip1)
+print('pyATS_Router 2 IP-Adres: ' + ip2)
+print('pyATS_Router 3 IP-Adres: ' + ip2)
