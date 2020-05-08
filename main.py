cdd9951719d325485e4815056e1f660828071d1b
timwijers <tim.wijers@student.poxl.be>
2020-05-08 11:02:07 +0200

    add api python file

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from IpExtractor import extractip
-
+import yaml
 
 ip1 = extractip('10.100.244.1', '45569', 'ethernet0/0')
 ip2 = extractip('10.100.244.1', '45570', 'ethernet0/0')
@@ -7,3 +7,7 @@ ip3 = extractip('10.100.244.1', '45571', 'ethernet0/0')
 print('pyATS_Router 1 IP-Adres: ' + ip1)
 print('pyATS_Router 2 IP-Adres: ' + ip2)
 print('pyATS_Router 3 IP-Adres: ' + ip3)
+
+with open('routerIOL_tb.yaml') as file:
+    loadedData = yaml.load(file, Loader=yaml.full_load())
+    print(loadedData)
