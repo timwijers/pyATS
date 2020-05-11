5e28419f9413b15f2a131fc476b667694f249314
timwijers <tim.wijers@student.poxl.be>
2020-05-11 17:27:15 +0200

    add api python file

--- Scratchfiles/testfile.py
+++ Scratchfiles/testfile.py
@@ -11,7 +11,7 @@ LoginUrl = 'http://10.100.244.1/api/auth/login'
 print(session.post(LoginUrl,LoginData).json())
 
 Url = 'http://10.100.244.1/api/labs'
-Data = {"path":"/Tim Wijers/pyATS_TestLabs/","name":"pyats111052021","version":"1",' \
+Data = {"path":"/Tim Wijers/pyATS_TestLabs/","name":timestamp,"version":"1",' \
                 '"author":"Tim Wijers","description":"pyATS Test Lab ","body":"This is a testlab created by the ' \
                 'Eve-NG REST API. Intended for pyATS framework testing purposes"}
 print(session.post(Url,None,Data).json())
