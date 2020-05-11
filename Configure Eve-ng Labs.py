dbbb5df91492bbd958bdb362bb9c7a5b82db1b73
timwijers <tim.wijers@student.poxl.be>
2020-05-11 17:09:56 +0200

    add api python file

--- Configure Eve-ng Labs.py
+++ Configure Eve-ng Labs.py
@@ -24,11 +24,8 @@ print(loginReq.json())
 
 # Uri's #
 AllLabsUrl = apiBaseUrl + '/labs'
-print(AllLabsUrl)
-PyATSTestLabUrl = AllLabsUrl + labsAuthor + labsFolder + 'pyats111052020.unl'
-print(PyATSTestLabUrl)
+PyATSTestLabUrl = AllLabsUrl + labsAuthor + labsFolder + '/pyats111052020.unl'
 AllNodesUrl = PyATSTestLabUrl + '/nodes'
-print(AllNodesUrl)
 AllNetworksUrl = PyATSTestLabUrl + '/networks'
 
 # Stop and Wipe all nodes from previous run #
