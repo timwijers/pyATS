f44e9471bf40fd43b5ade58e3f02b236388fc39d
timwijers <tim.wijers@student.poxl.be>
2020-05-11 15:18:27 +0200

    add api python file

--- Scratchfiles/main.py
+++ Scratchfiles/main.py
@@ -1,4 +1,4 @@
-from IpExtractor import extractip
+from Scratchfiles.IpExtractor import extractip
 import yaml
 
 ip1 = extractip('10.100.244.1', '45569', 'ethernet0/0')
@@ -8,6 +8,6 @@ print('pyATS_Router 1 IP-Adres: ' + ip1)
 print('pyATS_Router 2 IP-Adres: ' + ip2)
 print('pyATS_Router 3 IP-Adres: ' + ip3)
 
-with open('routerIOL_tb.yaml') as file:
+with open('../routerIOL_tb.yaml') as file:
     loadedData = yaml.load(file, Loader=yaml.)
     print(loadedData)
